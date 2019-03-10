b550a39e9d2ce6ca6083fa8f12c593f0787182d5
baruch <baruch@dinosaur.compy.com>
2019-03-10 16:13:42 +0200

    added mon_fri_flag times_open_tz to testing.sql

--- sql/internship/testing.sql
+++ sql/internship/testing.sql
@@ -1,12 +1,20 @@
 with step_1 as
 (
 select   replace
-            (
-             replace(field_6,' - ','-'),
-             ':',''
+             (
+              replace(field_6,' - ','-'),
+              ':',''
              ) no_dash_space
-FROM `data-science-course-226116.sql_lessons.stock_exchanges_raw_input`
+from    `data-science-course-226116.sql_lessons.stock_exchanges_raw_input`
 )
 
-select   no_dash_space
-from step_1
+select   no_dash_space,
+         -- --------------------------------------
+         case when lower(no_dash_space) like 'mon%'
+             then 'Y'
+             else 'N'
+         end mon_fri_flag,
+         -- --------------------------------------
+         trim(replace(lower(no_dash_space),'monday-friday','')) times_open_tz
+         -- --------------------------------------
+from     step_1
