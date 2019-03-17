e47139d9c977c0a3be1b7653e19a580df6585fd9
baruch <baruch@dinosaur.compy.com>
2019-03-17 13:19:57 +0200

    completed step 00

---
+++ sql/sql-hw-tasks/task-7.sql
@@ -0,0 +1,7 @@
+select   distinct field_6,
+         case when lower(field_6) like '%monday - friday%' 
+              then 'Y'
+              else 'N'
+         end step_00_mon_fri_flag
+from     `data-science-course-226116.sql_lessons.stock_exchanges_raw_input` 
+order by 1
