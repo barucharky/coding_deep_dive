cb7c1ef0f3152e1e102ee9edcc19cbd031c6f1f6
baruch <baruch@dinosaur.compy.com>
2019-03-17 14:38:00 +0200

    step 4 complete

--- sql/sql-hw-tasks/task-7.sql
+++ sql/sql-hw-tasks/task-7.sql
@@ -23,11 +23,17 @@ step_02 as
 select   *,
          strpos(step_01_remove_mon_fri,':') step_02_str_pos_of_colon
 from     step_01
-)
-
+),
+step_3 as
+(
 select   *,
          case when step_02_str_pos_of_colon > 0
              then substr(step_01_remove_mon_fri, 1, step_02_str_pos_of_colon - 1 ) 
              else ''
          end step_03_str_up_to_first_colon
 from     step_02
+)
+
+select   *,
+         safe_cast(step_03_str_up_to_first_colon as int64) step_04_str_up_to_first_colon_as_int64
+from     step_3
