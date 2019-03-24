e24101f3a7e43c52ca53549271bce9d992a3c0cf
baruch <baruch@dinosaur.compy.com>
2019-03-24 13:58:24 +0200

    renames

--- sql/sql-hw-tasks/task-004-complete.sql
+++ sql/sql-hw-tasks/task-004-complete.sql
@@ -1,5 +1,16 @@
 -- B''H --
 
+/*
+Show columns in result:
+    - county name (up to 'city' or up to 'County')
+    - first digit of the county name
+    - county fips code (cast to integer)
+    
+where state abbreviation starts with a, d, n, o, p
+
+order by county fips code
+*/
+
 select 
        -- ------------------------------------------
        case
