c550285aa69f90339687381d78a5dd4a66fcd605
baruch <baruch@dinosaur.compy.com>
2019-03-26 16:43:58 +0200

    worked on task-012-07

--- sql/sql-hw-tasks/task-012-07.sql
+++ sql/sql-hw-tasks/task-012-07.sql
@@ -1,6 +1,6 @@
 -- B''H --
 
-with step_00 as
+with stg_00 as
 (
 select   distinct field_6,
          case when lower(field_6) like 'monday - friday: %'
@@ -12,7 +12,7 @@ from     `data-science-course-226116.sql_lessons.stock_exchanges_raw_input`
 order by 1
 ),
 
-step_01 as
+stg_01 as
 (
 select   *,
          case when step_00_mon_fri_flag = 'Y'
@@ -20,28 +20,28 @@ select   *,
               else trim(field_6)
          -- ---------------------------------------------------------------
          end  step_01_remove_mon_fri
-from     step_00
+from     stg_00
 ),
 
-step_02 as
+stg_02 as
 (
 select   *,
          strpos(step_01_remove_mon_fri, ':') step_02_str_pos_of_colon
          -- ---------------------------------------------------------------
-from     step_01
+from     stg_01
 ),
 
-step_03 as
+stg_03 as
 (
 select   *,
          case when step_02_str_pos_of_colon > 0
               then substr(step_01_remove_mon_fri, 1, step_02_str_pos_of_colon - 1)
               else null
          end step_03_str_up_to_first_colon
-from     step_02
+from     stg_02
 ),
 
-step_04 as
+stg_04 as
 (
 select   *,
          safe_cast(step_03_str_up_to_first_colon as int64) step_04_str_up_to_first_colon_as_int64,
@@ -49,8 +49,65 @@ select   *,
               then step_03_str_up_to_first_colon
               else null
          end  step_05_times_prefix
-from     step_03
+from     stg_03
+),
+
+stg_05 as
+(
+select   *,
+         case when step_05_times_prefix is not null
+              then trim(
+                      replace(
+                           step_01_remove_mon_fri, 
+                           concat(step_05_times_prefix, ':'), 
+                           '')
+                       )
+              else step_01_remove_mon_fri
+         end step_06_remove_times_prefix
+from     stg_04
+),
+
+stg_06 as
+(
+select   *,
+         replace(step_06_remove_times_prefix, ' - ', '-') step_07_replace_blank_dash_blank
+from     stg_05
+),
+
+stg_07 as
+(
+select   *,
+         strpos(step_07_replace_blank_dash_blank, ':00 ') step_08_str_pos_of_colon_zero_zero_blank
+from     stg_06
+),
+
+stg_08 as
+(
+select   *,
+         case when step_08_str_pos_of_colon_zero_zero_blank > 0
+              then substr(step_07_replace_blank_dash_blank, step_08_str_pos_of_colon_zero_zero_blank + 4)
+              else null
+         end step_09_times_suffix
+from     stg_07
+),
+
+stg_09 as
+(
+select   *,
+         case when step_09_times_suffix is not null
+              then trim(replace(step_07_replace_blank_dash_blank, step_09_times_suffix, ''))
+              else step_07_replace_blank_dash_blank
+         end step_10_remove_times_suffix
+from     stg_08
+),
+
+stg_10 as 
+(
+select   *,
+         replace(step_10_remove_times_suffix, ':', '') step_11_remove_colons,
+         strpos(replace(step_10_remove_times_suffix, ':', ''), '/') step_12_str_pos_of_slash
+from     stg_09
 )
 
 select   *
-from     step_04
+from     stg_10
