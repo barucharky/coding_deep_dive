52412cde41d46ad9aac6b9888c74fad5a4936fb5
baruch <baruch@dinosaur.compy.com>
2019-03-31 13:22:25 +0300

    did task 13

---
+++ sql/sql-hw-tasks/task-013-complete.sql
@@ -0,0 +1,12 @@
+-- B''H --
+
+select   c.county_name,
+         c.fips_class_code,
+         s.state_name
+         -- ------------------------------------------
+from     `data-science-course-226116.sql_lessons.join_test_counties` c
+   left outer join `data-science-course-226116.sql_lessons.join_test_states` s
+     on c.state_abbreviation = s.state_abbreviation
+         -- ------------------------------------------
+where    s.state_name is null
+  and    lower(c.county_name) like '%ada%'
