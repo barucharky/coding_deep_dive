cb1232487d08294743012dd528f2d73e7eb930f2
baruch <baruch@dinosaur.compy.com>
2019-04-01 16:32:13 +0300

    mendels and task015 changes

--- sql/sql-hw-tasks/task-015-complete.sql
+++ sql/sql-hw-tasks/task-015-complete.sql
@@ -6,8 +6,8 @@ select   states.state_name,
          sum(usa_names.number) number_of_babies
 from     `bigquery-public-data.usa_names.usa_1910_current` usa_names
          -- ------------------------------------------------------
-   left outer join `data-science-course-226116.sql_lessons.states` states
-     on usa_names.state = states.state_abbreviation
+   left  outer join `data-science-course-226116.sql_lessons.states` states
+     on  usa_names.state = states.state_abbreviation
          -- ------------------------------------------------------
 group by states.state_name, usa_names.gender, usa_names.name
 having   number_of_babies > 370000
