5c037d553d6524bf83eb5fadfe4243f5e04edc04
baruch <baruch@dinosaur.compy.com>
2019-04-07 20:03:43 +0300

    fixed level of grain

--- sql/assignments-complete/task-018-complete.sql
+++ sql/assignments-complete/task-018-complete.sql
@@ -4,28 +4,28 @@ with
 year_names as 
 (
 select   year,
-         number,
-         sum(number) over (partition by year) number_of_names
+         sum(number) number_of_names
          -- ----------------------------------------------
 from     `bigquery-public-data.usa_names.usa_1910_current`
+         -- ----------------------------------------------
 where    lower(name) in ('mendy', 'mendi', 'mendel', 'menachem')
-  and    gender = 'M'
-)
-
-, numbers as
-(
-select   year,
-         number_of_names,
-         sum(number_of_names) over(
-           rows between unbounded preceding and current row
-         ) cumulative_number_of_names,
-         sum(number) over() total_number_of_names
-from     year_names
+  and    lower(gender) = 'm'
+         -- ----------------------------------------------
+group by year
 )
 
 select   *,
+         sum(number_of_names) over (
+             order by      year
+             range between unbounded preceding and current row
+         ) cumulative_number_of_names,
+         -- ----------------------------------------------
+         sum(number_of_names) over () total_number_of_names,
+         -- ----------------------------------------------
          round(
-           (number_of_names/sum(total_number_of_names) over (partition by year)) * 100,
-           2
+             (number_of_names/sum(number_of_names) over()) * 100,
+             2
          ) year_pct_from_total
-from     numbers
+         -- ----------------------------------------------
+from     year_names
+order by year
