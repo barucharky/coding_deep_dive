a4aaa8928c016b2418afc34ba4c526f3aba1e834
baruch <baruch@dinosaur.compy.com>
2019-05-05 15:08:35 +0300

    finished task-21g.sql - ice cream topping combos

--- sql/assignments/sql-task-021/task-021g.sql
+++ sql/assignments/sql-task-021/task-021g.sql
@@ -58,45 +58,107 @@ select   kids.name   name,
 from     kids
    cross join snacks
 
--- I can't figure out a way to get rid of duplicates. Maybe this isn't a case for a cross join
+-- All possible combinations of toppings for ice cream
 
 with
-ice_cream as
+-- Here are all available toppings
+
+toppings as
 (
 select   'fudge'         topping union all
 select   'caramel'       topping union all
 select   'nuts'          topping union all
 select   'sprinkles'     topping union all
-select   'whipped cream' topping union all
+select   'whip'          topping union all
 select   null            topping
 ),
 
+
+/*
+_______________________________________________________________________
+here are all possible combinations
+each id represents a customer with his set of toppings accross 5 fields
+('fudge', 'fudge') is the same as ('fudge', null)
+_______________________________________________________________________
+*/
 combos as
 (
-select   distinct
-         s1.topping s1_topping,
-         s2.topping s2_topping,
-         s3.topping s3_topping,
-         s4.topping s4_topping,
-         s5.topping s5_topping
-from     ice_cream s1
-   cross join ice_cream s2
-   cross join ice_cream s3
-   cross join ice_cream s4
-   cross join ice_cream s5
+select   row_number() over () id,
+         s1.topping           s1_topping,
+         s2.topping           s2_topping,
+         s3.topping           s3_topping,
+         s4.topping           s4_topping,
+         s5.topping           s5_topping
+         -- -----------------------------
+from     toppings s1
+   cross join toppings s2
+   cross join toppings s3
+   cross join toppings s4
+   cross join toppings s5
+),
+
+
+/* 
+since each customer must have 5 toppings at this point (including nulls and duplicates)
+and they must be listed vertically (pivoted) each topping in a set (id) will have a number
+*/
+sets as
+(
+select   1 topping_num union all
+select   2             union all
+select   3             union all
+select   4             union all
+select   5        
+),
+
+
+/*
+each combination listed by topping number and id
+*/
+combos_unpivoted as
+(
+select   id,
+         topping_num,
+         case topping_num
+              when 1 then s1_topping
+              when 2 then s2_topping
+              when 3 then s3_topping
+              when 4 then s4_topping
+              when 5 then s5_topping
+         end topping_value
+         -- ------------------------              
+from     combos
+   cross join sets
+),
+
+
+/*
+here they are grouped by id and aggragated by distinct topping_value which is ordered.
+this means that now, both ('fudge', 'fudge') and ('fudge', null) and (null, 'fudge')
+will all become 'fudge'
+
+distinct changes ('fudge', 'fudge') to 'fudge'
+order by makes ('fudge', null) and (null, 'fudge') the same
+the null is lost in aggragation
+*/
+combos_csv as
+(
+select   id,
+         -- -----------------------------
+         string_agg(
+             distinct topping_value, ', '
+             order by topping_value
+         ) topping_values
+         -- -----------------------------
+from     combos_unpivoted
+group by id
 )
 
-select   *
-from     combos
-where    concat(coalesce(s1_topping, '<NO_TOP>'), coalesce(s2_topping, '<NO_TOP>'), coalesce(s3_topping, '<NO_TOP>'), coalesce(s4_topping, '<NO_TOP>'), coalesce(s5_topping, '<NO_TOP>')) not like '%lettuce%lettuce%' 
-  and    concat(coalesce(s1_topping, '<NO_TOP>'), coalesce(s2_topping, '<NO_TOP>'), coalesce(s3_topping, '<NO_TOP>'), coalesce(s4_topping, '<NO_TOP>'), coalesce(s5_topping, '<NO_TOP>')) not like '%tomato%tomato%' 
-  and    concat(coalesce(s1_topping, '<NO_TOP>'), coalesce(s2_topping, '<NO_TOP>'), coalesce(s3_topping, '<NO_TOP>'), coalesce(s4_topping, '<NO_TOP>'), coalesce(s5_topping, '<NO_TOP>')) not like '%cheese%cheese%' 
-  and    concat(coalesce(s1_topping, '<NO_TOP>'), coalesce(s2_topping, '<NO_TOP>'), coalesce(s3_topping, '<NO_TOP>'), coalesce(s4_topping, '<NO_TOP>'), coalesce(s5_topping, '<NO_TOP>')) not like '%onion%onion%' 
-  and    concat(coalesce(s1_topping, '<NO_TOP>'), coalesce(s2_topping, '<NO_TOP>'), coalesce(s3_topping, '<NO_TOP>'), coalesce(s4_topping, '<NO_TOP>'), coalesce(s5_topping, '<NO_TOP>')) not like '%mustard%mustard%'
-  and    concat(coalesce(s1_topping, '<NO_TOP>'), coalesce(s2_topping, '<NO_TOP>'), coalesce(s3_topping, '<NO_TOP>'), coalesce(s4_topping, '<NO_TOP>'), coalesce(s5_topping, '<NO_TOP>')) not like '%<NO_TOP>%l%'
-  and    concat(coalesce(s1_topping, '<NO_TOP>'), coalesce(s2_topping, '<NO_TOP>'), coalesce(s3_topping, '<NO_TOP>'), coalesce(s4_topping, '<NO_TOP>'), coalesce(s5_topping, '<NO_TOP>')) not like '%<NO_TOP>%t%'
-  and    concat(coalesce(s1_topping, '<NO_TOP>'), coalesce(s2_topping, '<NO_TOP>'), coalesce(s3_topping, '<NO_TOP>'), coalesce(s4_topping, '<NO_TOP>'), coalesce(s5_topping, '<NO_TOP>')) not like '%<NO_TOP>%c%'
-  and    concat(coalesce(s1_topping, '<NO_TOP>'), coalesce(s2_topping, '<NO_TOP>'), coalesce(s3_topping, '<NO_TOP>'), coalesce(s4_topping, '<NO_TOP>'), coalesce(s5_topping, '<NO_TOP>')) not like '%<NO_TOP>%o%'
-  and    concat(coalesce(s1_topping, '<NO_TOP>'), coalesce(s2_topping, '<NO_TOP>'), coalesce(s3_topping, '<NO_TOP>'), coalesce(s4_topping, '<NO_TOP>'), coalesce(s5_topping, '<NO_TOP>')) not like '%<NO_TOP>%m%'
-  and    s1_topping <> '<NO_TOP>'
-order by 1, 2, 3, 4, 5
+
+/*
+because ('fudge', 'fudge') and ('fudge', null) and (null, 'fudge') all aggragate
+to 'fudge', there are now duplicates. this is resolved by distinct
+*/
+select   distinct topping_values
+from     combos_csv
+order by 1
