c8294396ba93efa8f8428122058bcd1db61678d7
baruch <baruch@dinosaur.compy.com>
2019-05-26 10:03:33 +0300

    deleted duplicate folders

--- sql/assignments/sql-task-021/internship/main.sql
+++
@@ -1,8 +0,0 @@
-select   field_2 name,
-         field_3 symbol,
-         field_4 continent,
-         field_5 country
-         -- ---------------------------
-from `data-science-course-226116.sql_lessons.stock_exchanges_raw_input`
-         -- ---------------------------
-where field_4 in ('Americas', 'Europe', 'Asia')
